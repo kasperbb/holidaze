082be6ed04dd08e275f7076612426914ddd9ae3e
kasperbb <kasperbb98@gmail.com>
2022-04-14 18:53:53 +0200

    try again

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,22 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import { GoTrueClient } from '@supabase/supabase-js'
-
-const authClient = new GoTrueClient({
-  url: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1`,
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
-    apikey: `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
-  },
-  cookieOptions: {
-    name: 'sb:token',
-    lifetime: 60 * 60 * 8,
-    domain: '',
-    path: '/',
-    sameSite: 'lax',
-  },
-})
+import { supabase } from '@lib/supabase'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  authClient.api.setAuthCookie(req, res)
+  supabase.auth.api.setAuthCookie(req, res)
 }
