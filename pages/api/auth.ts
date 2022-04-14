c5edd3548ebdbd51d65cb10740c2346114d319d5
kasperbb <kasperbb98@gmail.com>
2022-04-14 18:39:45 +0200

    Adds GoTrueClient to api auth route

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,9 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import { createClient } from '@supabase/supabase-js'
+import { GoTrueClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL ?? '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '')
+const authClient = new GoTrueClient({
+  url: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1`,
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+    apikey: `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+  },
+  cookieOptions: {
+    name: 'sb:token',
+    lifetime: 60 * 60 * 8,
+    domain: '',
+    path: '/',
+    sameSite: 'lax',
+  },
+})
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  supabase.auth.api.setAuthCookie(req, res)
+  authClient.api.setAuthCookie(req, res)
 }
