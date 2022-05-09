087a615bd3f51b3ec1b7ee51dd89f33229eb8b6a
kasperbb <kasperbb98@gmail.com>
2022-05-09 16:00:57 +0200

    Adds error message and fallback if map fails to load

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,8 +1,8 @@
-import { CSSProperties, forwardRef } from 'react'
+import { Alert, AlertDescription, AlertIcon, AlertTitle, HStack, chakra, useToken } from '@chakra-ui/react'
+import { CSSProperties, forwardRef, useState } from 'react'
 import ReactMapGL, { FullscreenControl, MapRef, Marker, MarkerProps, NavigationControl, Popup, PopupProps } from 'react-map-gl'
 
 import maplibregl from 'maplibre-gl'
-import { useToken } from '@chakra-ui/react'
 
 interface MapProps {
   lat?: number
@@ -16,46 +16,70 @@ interface MapProps {
 }
 
 export const Map = forwardRef<MapRef, MapProps>(({ lat = 60.3914191, long = 5.3248788, zoom = 13, style, popupList, markerList, hasBorder, ...rest }, ref) => {
+  const [error, setError] = useState<string | null>(null)
   const xlBroderRadius = useToken('radii', '2xl')
   const mdBorderRadius = useToken('radii', 'md')
+  const shadow = useToken('shadows', 'primary')
 
   return (
-    <ReactMapGL
-      mapLib={maplibregl}
-      mapStyle="https://api.maptiler.com/maps/streets/style.json?key=aWayibDnh8VTw3ovokeu"
-      attributionControl={false}
-      initialViewState={{
-        latitude: lat,
-        longitude: long,
-        zoom: zoom,
-      }}
-      style={{
-        height: 330,
-        borderRadius: hasBorder ? xlBroderRadius : mdBorderRadius,
-        boxShadow: useToken('shadows', 'primary'),
-        border: hasBorder ? '10px solid white' : 'none',
-        ...style,
-      }}
-      ref={ref}
-      {...rest}
-    >
-      {popupList &&
-        popupList.map(popup => (
-          <Popup key={popup.latitude + popup.longitude} {...popup}>
-            {popup.children}
-          </Popup>
-        ))}
+    <>
+      {error ? (
+        <>
+          <Alert status="error" borderRadius="lg">
+            <AlertIcon />
+            <AlertTitle>Error!</AlertTitle>
+            <AlertDescription>{error}</AlertDescription>
+          </Alert>
 
-      {markerList &&
-        markerList.map(marker => (
-          <Marker key={marker.latitude + marker.longitude} {...marker}>
-            {marker.children}
-          </Marker>
-        ))}
+          <HStack mt={4}>
+            <chakra.dt minWidth={10}>Latitude:</chakra.dt>
+            <chakra.dd fontWeight="semibold">{lat}</chakra.dd>
+          </HStack>
+          <HStack>
+            <chakra.dt minWidth={10}>Longitude:</chakra.dt>
+            <chakra.dd fontWeight="semibold">{long}</chakra.dd>
+          </HStack>
+        </>
+      ) : (
+        <ReactMapGL
+          onError={({ error }) => setError(error.message)}
+          mapLib={maplibregl}
+          mapStyle="https://api.maptiler.com/maps/streets/style.json?key=aWayibDnh8VTw3ovokeu"
+          attributionControl={false}
+          initialViewState={{
+            latitude: lat,
+            longitude: long,
+            zoom: zoom,
+          }}
+          style={{
+            height: 330,
+            borderRadius: hasBorder ? xlBroderRadius : mdBorderRadius,
+            boxShadow: shadow,
+            border: hasBorder ? '10px solid white' : 'none',
+            ...style,
+          }}
+          ref={ref}
+          {...rest}
+        >
+          {popupList &&
+            popupList.map(popup => (
+              <Popup key={popup.latitude + popup.longitude} {...popup}>
+                {popup.children}
+              </Popup>
+            ))}
 
-      <NavigationControl />
-      <FullscreenControl />
-    </ReactMapGL>
+          {markerList &&
+            markerList.map(marker => (
+              <Marker key={marker.latitude + marker.longitude} {...marker}>
+                {marker.children}
+              </Marker>
+            ))}
+
+          <NavigationControl />
+          <FullscreenControl />
+        </ReactMapGL>
+      )}
+    </>
   )
 })
 
