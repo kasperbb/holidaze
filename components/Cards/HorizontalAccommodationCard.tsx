b90cf087c141464c7d326d1ed3336ed38d34afb9
kasperbb <kasperbb98@gmail.com>
2022-05-13 17:33:38 +0200

    Adds link to entire horizontal accommodation card

--- components/Cards/HorizontalAccommodationCard.tsx
+++ components/Cards/HorizontalAccommodationCard.tsx
@@ -20,40 +20,39 @@ export function HorizontalAccommodationCard({ id, name, images, price, rating, u
   const shouldShowActionsButton = showEditButton && (user?.id === user_id || user?.role === 'admin')
 
   return (
-    <Card
-      key={id}
-      variant="horizontal"
-      imageSrc={images ? images[0].url : '/placeholder.png'}
-      imageAlt="Holidaze"
-      mb={4}
-      contentProps={{ width: 'full', display: 'flex', flexDirection: 'column' }}
-    >
-      <Flex align="center" justify="space-between" gap={10}>
-        <NextLink href={`/accommodations/${id}`} passHref>
-          <Link display="block" mb={4}>
-            <Heading as="h3" fontSize="25px" fontWeight="semibold">
-              {name}
-            </Heading>
-          </Link>
-        </NextLink>
-        {shouldShowActionsButton && <ActionsButton id={id} featured={featured} name={name} />}
-      </Flex>
-
-      <Flex align="center" justify="space-between" gap={10}>
-        <Flex align="center" color="orange.800" fontWeight="bold" fontSize="sm" gap={2} aria-label="Average rating">
-          <Badge display="flex" alignItems="center" fontSize="sm" borderRadius="md" colorScheme="orange" px={1}>
-            {averageRating}/5
-          </Badge>
-          Average
+    <NextLink href={`/accommodations/${id}`} passHref>
+      <Card
+        key={id}
+        as="a"
+        variant="horizontal"
+        imageSrc={images ? images[0].url : '/placeholder.png'}
+        imageAlt="Holidaze"
+        mb={4}
+        contentProps={{ width: 'full', display: 'flex', flexDirection: 'column' }}
+      >
+        <Flex align="center" justify="space-between" gap={10}>
+          <Heading as="h3" fontSize="25px" fontWeight="semibold">
+            {name}
+          </Heading>
+          {shouldShowActionsButton && <ActionsButton id={id} featured={featured} name={name} />}
         </Flex>
-        <Text fontSize="20px" fontWeight="semibold" color="success.500" whiteSpace="nowrap">
-          €{price}
-          <chakra.span fontSize="14px" color="text.secondary" fontWeight="normal" ml={2}>
-            per night
-          </chakra.span>
-        </Text>
-      </Flex>
-    </Card>
+
+        <Flex align="center" justify="space-between" gap={10}>
+          <Flex align="center" color="orange.800" fontWeight="bold" fontSize="sm" gap={2} aria-label="Average rating">
+            <Badge display="flex" alignItems="center" fontSize="sm" borderRadius="md" colorScheme="orange" px={1}>
+              {averageRating}/5
+            </Badge>
+            Average
+          </Flex>
+          <Text fontSize="20px" fontWeight="semibold" color="success.500" whiteSpace="nowrap">
+            €{price}
+            <chakra.span fontSize="14px" color="text.secondary" fontWeight="normal" ml={2}>
+              per night
+            </chakra.span>
+          </Text>
+        </Flex>
+      </Card>
+    </NextLink>
   )
 }
 
